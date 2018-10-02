5383fb4179f1aec972c5f2cc956a0fee07af353a
John Doe <jamez@hemail.com>
2018-10-02 23:04:13 +0200

    Add methods

--- user_repo.rb
+++ user_repo.rb
@@ -5,4 +5,18 @@ class UserRepo
     @users = database[:users]
   end
 
+  def user_exists?(identification)
+    !get_user_by_identification(identification).nil?
+  end
+
+  private
+
+  def get_user_by_identification(identification)
+    @users.where(phone: identification).or(email: identification).first
+  end
+
+  def hash(password)
+    password.reverse
+  end
+
 end
