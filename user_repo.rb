b26aed283d56c65845b02957a11d90bc091ac35a
John Doe <angelo_muh@yahoo.org>
2018-10-02 23:55:57 +0200

    Add login method

--- user_repo.rb
+++ user_repo.rb
@@ -5,6 +5,13 @@ class UserRepo
     @users = database[:users]
   end
 
+  def login(identification, password)
+    hashed_input_password = hash(password)
+    query = "select id, phone, email from users where email = '#{identification}' and password_digest = '#{hashed_input_password}' limit 1"
+    puts "SQL executing: '#{query}'"
+    @database[query].first if user_exists?(identification)
+  end
+
   def user_exists?(identification)
     !get_user_by_identification(identification).nil?
   end
